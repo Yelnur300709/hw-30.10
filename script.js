72fa014dc6654c0552620c93258fb1a994ef3e67
Yelnur300709 <mirasleha8@gmail.com>
2024-10-30 18:38:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = "Yelnur";
+var surname = "Tyrsingaly";
+var birthday = "30.07.2009";
+var otan = "Kazakhstan";
+
+console.log("Menin name", name);
+console.log("Menin surname", surname);
+console.log("Menin birthday ", birthday);
+console.log("Menin otan", otan);
